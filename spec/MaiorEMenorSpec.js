105131eab9a1343f8fa2ea31a2a1782cd438f98c
Álvaro Fernando <alvarofernandoms@gmail.com>
2018-02-05 20:29:24 -0200

    Add testes de exemplo

--- spec/MaiorEMenorSpec.js
+++ spec/MaiorEMenorSpec.js
@@ -7,5 +7,26 @@ describe('MaiorEMenor', () => {
 
     expect(algoritmo.pegaMaior()).toEqual(15);
     expect(algoritmo.pegaMenor()).toEqual(5);
+  }),
+  it('deve entender números em ordem crescente', () => {
+    var algoritmo = new MaiorEMenor();
+    algoritmo.encontra(5, 6, 7, 8);
+
+    expect(algoritmo.pegaMaior()).toEqual(8);
+    expect(algoritmo.pegaMenor()).toEqual(5);
+  })
+  it('deve entender números em ordem decrescente', () => {
+    var algoritmo = new MaiorEMenor();
+    algoritmo.encontra(8, 7, 6, 5);
+
+    expect(algoritmo.pegaMaior()).toEqual(8);
+    expect(algoritmo.pegaMenor()).toEqual(5);
+  }),
+  it('deve entender a entrada de um único elemento', () => {
+    var algoritmo = new MaiorEMenor();
+    algoritmo.encontra(8);
+
+    expect(algoritmo.pegaMaior()).toEqual(8);
+    expect(algoritmo.pegaMenor()).toEqual(8);
   })
 });
